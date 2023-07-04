234f0a7977007a0dd451c378753ca97ae78810b3
Alex Coaton <alex.coaton@gmail.com>
2023-07-05 11:59:57 +1200

    Bird rotates after flapping.

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bird : MonoBehaviour
@@ -10,11 +9,17 @@ public class Bird : MonoBehaviour
   private CharacterController controller;
   private Animator animator;
   private Vector3 velocity = new Vector3(0, 0, 0);
+  private const float flappingVelocity = 8f;
+  private Quaternion lookingForwardRotation;
+  private Quaternion lookingUpRotation;
+  private const float flappingPeriod = 0.5f;
+  private float flappingTimeElapsed = 0;
 
   void Start()
   {
     controller = gameObject.GetComponent<CharacterController>();
     animator = gameObject.GetComponent<Animator>();
+    lookingForwardRotation = transform.rotation;
   }
 
   void Update()
@@ -23,20 +28,28 @@ public class Bird : MonoBehaviour
 
     if (Input.GetKeyDown("space"))
     {
-      velocity.y = 8f;
+      velocity.y = flappingVelocity;
       animator.Play("Wings|flapping");
+
+      flappingTimeElapsed = 0f;
+      // Get looking up rotation
+      lookingUpRotation = Quaternion.LookRotation(lookUp.transform.position - transform.position);
+
       StartCoroutine(WaitThenPlayDefaultAnim());
       gameObject.transform.LookAt(lookUp.transform);
     }
+
+    flappingTimeElapsed += Time.deltaTime;
+    // SLERP between the quaternions
+    transform.rotation = Quaternion.Slerp(lookingUpRotation, lookingForwardRotation, flappingTimeElapsed / flappingPeriod);
 
     controller.Move(velocity * Time.deltaTime);
   }
 
   private IEnumerator WaitThenPlayDefaultAnim()
   {
-    yield return new WaitForSeconds(0.5f);
+    yield return new WaitForSeconds(flappingPeriod);
     animator.Play("Wings|flying");
-    gameObject.transform.LookAt(new Vector3(0, transform.position.y, transform.position.z + 10));
   }
 }
 
